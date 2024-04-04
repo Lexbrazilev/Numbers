5ec0b4696ada883cd84a1b6b3e5bb557c8e82242
Lexbrazilev <burtyt@mail.ru>
2024-04-04 10:54:38 +0300

    Добавьте файлы проекта.

---
+++ Numbers/Numbers.py
@@ -0,0 +1,11 @@
+﻿number = int(input("Введите число "))
+if (number < 0) and (number % 2 == 0):
+    print("Отрицательное четное число")
+elif (number < 0) and (number % 2 == 1):
+    print("Отрицательное нечетное число")
+elif (number > 0) and (number % 2 == 0):
+    print("Положительное четное число")
+elif (number > 0) and (number % 2 == 1):
+    print("Положительное нечетное число")
+elif number == 0:
+    print("Нулевое число")
